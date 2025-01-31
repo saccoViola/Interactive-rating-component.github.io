a6394e49f603d7e779caa8c5ca34113a95e9e2d8
saccoViola <168297914+saccoViola@users.noreply.github.com>
2025-01-31 17:20:33 +0100

    fixing accessibility tag

--- script.js
+++ script.js
@@ -13,11 +13,7 @@ const selectedNumber = document.getElementById("selected-number");
 
 submitButton.textContent = submitButton.textContent.toUpperCase();
 
-submitButton.addEventListener("click", ()=>{
-    firstContainer.style.display ="none";
-    secondContainer.style.display ="block";
 
-})
 
 // firstContainer.style.display = "none";
 
@@ -27,10 +23,47 @@ for (let i = 1; i <= 5; i++) {
     ratingButton.innerHTML = i; 
     buttons.appendChild(ratingButton);
     ratingButton.addEventListener("click", ()=>{
+        event.preventDefault();
         ratingButton.style.backgroundColor = "hsl(25, 97%, 53%)";
         ratingButton.style.color="hsl(213, 19%, 15%)";
         selectedNumber.innerHTML = "You selected " + i + " out of 5";
     })
+
+    submitButton.addEventListener("click", ()=>{
+        event.preventDefault();
+        firstContainer.style.display ="none";
+        document.getElementById("second-container").removeAttribute("aria-hidden");
+        secondContainer.style.display ="block";
+
+    })
 }
 
 
+
+/*
+alert if no rating button is clicked before clicking the submitButton
+
+
+se 
+
+
+if 
+ (ratingButton.addEventListener("click", ()=>{
+        ratingButton.style.backgroundColor = "hsl(25, 97%, 53%)";
+        ratingButton.style.color="hsl(213, 19%, 15%)";
+        selectedNumber.innerHTML = "You selected " + i + " out of 5";
+})){
+    submitButton.addEventListener("click", ()=>{
+    firstContainer.style.display ="none";
+    secondContainer.style.display ="block";
+        }
+
+
+
+    submitButton.addEventListener("click", ()=>{
+    firstContainer.style.display ="none";
+    secondContainer.style.display ="block";
+
+})
+
+If the user clicks on several rating buttons one after the other, they all turn orange. It would be more user friendly to have only the rating button that has been most recently clicked be orange*/ 
